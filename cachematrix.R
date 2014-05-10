9a8f10fe42c30f34084c9c8e6759c4ef224368d6
Antonio Ferreras <ferreras@ferrerasdell.hi.inet>
2014-05-10 18:51:42 +0200

    Final Version

--- cachematrix.R
+++ cachematrix.R
@@ -1,19 +1,23 @@
+##
 ## R PROGRAMING Course
 ## Programing Assigment 2
-## Created: 5 May, 2014
-## Last modified: 5th May, 2014
+## Created: 5th May, 2014
+## Last modified: 6th May, 2014
 ## Author: Antonio F.
 
 ##
-
 ## Creates a special matrix, really a lit containing 4 functions
 ##      $set : set matrix value
 ##      $get : get matrix value
 ##      $setinverse : set inverse matrix value
 ##      $getinverse ; get inverse matrix value
+##
 
 makeCacheMatrix <- function(a = matrix()) {
+    
     inv <- NULL
+    
+    ## Define the 4 functions
     set <- function(b) {
         a <<- b
         inv <<- NULL
@@ -21,6 +25,8 @@ makeCacheMatrix <- function(a = matrix()) {
     get <- function() a
     setinverse <- function(inverse) inv <<- inverse
     getinverse <- function() inv
+    
+    ## Return a formated list with the four functions
     list(set = set, get = get,
          setinverse = setinverse,
          getinverse = getinverse)
@@ -32,16 +38,16 @@ makeCacheMatrix <- function(a = matrix()) {
 ## the inverse from the cache, otherwise calculates, stores and returns
 ## the inverse
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(a, ...) {
     
-    ## Try to get te stored value of the inverse of 'x'
-    inv <- x$getinverse()
-    if(!is.null(inv)) {
-        message("getting cached data")
-    } else {
-        message("solving")
-        inv <- solve(x$get())
-        x$setinverse(inv)
+    ## Try to get te stored value of the inverse of 'a'
+    inv <- a$getinverse()
+    
+    ## If not exists then calculate and store the inverse
+    if(is.null(inv)) {
+        inv <- solve(a$get())
+        a$setinverse(inv)
     }
+    
     return(inv)
 }
